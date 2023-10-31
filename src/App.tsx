5862cbc9717d94c653880ee67f556e2f32a07cce
web-lux <web-lux@outlook.fr>
2023-10-31 17:08:11 +0100

    feat: style main

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
 import Header from "./Components/Header";
+import Main from "./Components/Main";
 
 function App() {
 	return (
 		<div className="wrapper">
 
 			<Header />
-			
-			<main>
-				<div className="main-infos">
-					<img src="/weather-img/clear.png" alt="" />
-					<div className="data">
-						<div className="data-temp">
-							<span>23°</span>
-							<button className="c-button">C°</button>
-						</div>
-						<span className="data-weather">Nuageux</span>
-					</div>
-				</div>
-
-				<div className="city">Saint-Amand-Les-Eaux</div>
-
-				<ul className="details">
-					<li>
-						<div className="header">
-							<img src="/icons/thermometer.svg" alt="" />
-							<h3>Se ressent comme</h3>
-						</div>
-						<span className="data">21°</span>
-					</li>
-				</ul>
-			</main>
+			<Main />
 		</div>
 	);
 }
---
+++ src/Components/Main.tsx
@@ -0,0 +1,46 @@
+import style from "./Main.module.scss";
+
+export default function Main() {
+	return (
+		<main>
+			<div className={style.mainInfos}>
+				<img src="/weather-img/clear.png" alt="" />
+				<div>
+					<div className={style.dataTemp}>
+						<span>23°</span>
+						<button className="c-button">C°</button>
+					</div>
+					<span className={style.dataWeather}>Nuageux</span>
+				</div>
+			</div>
+
+			<div className={style.city}>Saint-Amand-Les-Eaux</div>
+
+			<ul className={style.details}>
+				<li>
+					<div className={style.detailsHeader}>
+						<img src="/icons/thermometer.svg" alt="" />
+						<h3>Se ressent comme</h3>
+					</div>
+					<span className={style.detailsData}>21°</span>
+				</li>
+
+				<li>
+					<div className={style.detailsHeader}>
+						<img src="/icons/thermometer.svg" alt="" />
+						<h3>Se ressent comme</h3>
+					</div>
+					<span className={style.detailsData}>21°</span>
+				</li>
+
+				<li>
+					<div className={style.detailsHeader}>
+						<img src="/icons/thermometer.svg" alt="" />
+						<h3>Se ressent comme</h3>
+					</div>
+					<span className={style.detailsData}>21°</span>
+				</li>
+			</ul>
+		</main>
+	);
+}
