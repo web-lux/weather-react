0e82f20b5bfc763cde042c4f0061b0d3b1199fb3
web-lux <web-lux@outlook.fr>
2023-10-31 13:18:00 +0100

    feat: style header

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,18 @@
+import Header from "./Components/Header";
+
 function App() {
 	return (
 		<div className="wrapper">
-			<header>
-				<form action="/" method="get">
-					<label htmlFor="searched-city" id="search-label" hidden>
-						Rechercher une ville :
-					</label>
-					<input
-						type="text"
-						name="searched-city"
-						id="searched-city"
-						placeholder="Rechercher une ville..."
-						required
-						aria-labelledby="search-label"
-					/>
-					<button type="submit" aria-label="Rechercher ville"></button>
-				</form>
 
-				<div className="watched-cities">
-					<div className="label">
-						<h2 id="watched-cities-title">Villes suivies</h2>
-						<button aria-label="Ajouter la ville actuelle aux villes suivies"></button>
-					</div>
-					<nav aria-labelledby="watched-cities-title">
-						<ul>
-							<li>Nantes</li>
-							<li>Londres</li>
-							<li>Paris</li>
-						</ul>
-					</nav>
-				</div>
-			</header>
-
+			<Header />
+			
 			<main>
 				<div className="main-infos">
 					<img src="/weather-img/clear.png" alt="" />
 					<div className="data">
 						<div className="data-temp">
 							<span>23°</span>
-							<button>C°</button>
+							<button className="c-button">C°</button>
 						</div>
 						<span className="data-weather">Nuageux</span>
 					</div>
---
+++ src/Components/Header.tsx
@@ -0,0 +1,43 @@
+import style from "./Header.module.scss";
+
+export default function Header() {
+	return (
+		<header className={`${style.header} glass`}>
+			<form action="/" method="get">
+				<label htmlFor="search-city" id="search-label" hidden>
+					Rechercher une ville :
+				</label>
+				<input
+					type="text"
+					name="search-city"
+					id="search-city"
+					placeholder="Rechercher une ville..."
+					aria-labelledby="search-label"
+					required
+				/>
+				<button type="submit" aria-label="Rechercher ville">
+					<img src="/icons/paper-plane.svg" alt="" />
+				</button>
+			</form>
+
+			<div className={style.watchedCities}>
+				<div className={style.label}>
+					<h2 id="watched-cities-title">Villes suivies</h2>
+					<button
+						aria-label="Ajouter la ville actuelle aux villes suivies"
+						className="c-button"
+					>
+						<img src="/icons/eye-plus.svg" alt="" />
+					</button>
+				</div>
+				<nav aria-labelledby="watched-cities-title">
+					<ul>
+						<li>Nantes</li>
+						<li>Londres</li>
+						<li>Paris</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+	);
+}
