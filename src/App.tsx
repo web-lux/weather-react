a630b6b1c9a51dc07e3d7aa57ad6f9d86697d795
web-lux <web-lux@outlook.fr>
2023-11-03 14:18:55 +0100

    feat: get user coords upon arrival

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
+import toast from "react-hot-toast";
 
 function App() {
+
+	function getCoords() {
+		navigator.geolocation.getCurrentPosition((pos) => {
+			console.warn(pos.coords)
+		}, () => {
+			toast.error(`Une erreur s'est produite avec la géolocalisation. Par défaut, votre ville a été définie comme Paris.`);
+		})
+	};
+
+	useEffect(() => {
+		getCoords();
+	}, []);
+
 	return (
 		<div className="wrapper">
 			<Header />
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./normalize.css";
 import "./index.scss";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<App />
+		<Toaster />
 	</React.StrictMode>
 );
